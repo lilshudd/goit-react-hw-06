1f91a59390b1f6a7906d3b99f0a94f3b4e51a4c2
lilshudd <max.fedorak10@gmail.com>
2024-03-16 16:01:32 +0200

    do

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store";
 import ContactForm from "./components/ContactForm";
 import SearchBox from "./components/SearchBox";
 import ContactList from "./components/ContactList";
@@ -8,16 +5,12 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div className="container">
-          <h1>Phonebook</h1>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-        </div>
-      </PersistGate>
-    </Provider>
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 };
 
